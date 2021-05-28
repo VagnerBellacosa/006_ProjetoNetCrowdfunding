430d59c6505da874f9c2d5ae219eab552e46e607
Vagner Bellacosa <83479901+VagnerBellacosa@users.noreply.github.com>
2021-05-28 18:12:02 -0300

    Primeira Guarda
    Arquivos iniciais

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _006_ProjetoNetCrowdfunding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
